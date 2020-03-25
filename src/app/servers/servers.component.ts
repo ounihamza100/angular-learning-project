d3d5fce530d51fb4f12f85c2064a2cf7ff58aba7
unknown <hamza.ellouni@gmail.com>
2020-03-25 18:53:53 +0100

    one way bindig

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,18 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   // selector: 'app-servers',
   // selector: '[app-servers]',
-  // tslint:disable-next-line:component-selector
-  selector: '.app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
+  selector: 'app-servers',
+  templateUrl : './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNwServer = false;
+  serverCreationStatus = 'No server was created!';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNwServer = true;
+      }, 2000);
+  }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus ='Server was created!';
   }
 
 }
